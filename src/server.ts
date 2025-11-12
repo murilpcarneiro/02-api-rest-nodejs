8d4fb03e459ae74b5cecb32cc753b3f8f8185414
Murilo <murilpcarneiro@gmail.com>
2025-11-12 01:13:46 -0300

    refactor: enhance server startup configuration with host specification

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,9 @@ import { env } from './env'
 
 const PORT = env.PORT || 3333
 
-app.listen({ port: PORT }).then(() => console.log(`The server is running`))
+app
+  .listen({
+    port: PORT,
+    host: '0.0.0.0',
+  })
+  .then(() => console.log(`The server is running on port ${PORT}`))
