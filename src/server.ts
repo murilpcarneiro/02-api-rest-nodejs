a2db39f212570a01ec1d845430f241b4b60eba7d
Murilo <murilpcarneiro@gmail.com>
2025-11-12 01:03:03 -0300

    fix: set default PORT value to 3333 if not specified in environment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { app } from './app'
 import { env } from './env'
 
-const PORT = env.PORT
+const PORT = env.PORT || 3333
 
 app.listen({ port: PORT }).then(() => console.log(`The server is running`))
